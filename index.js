3a7201506bb2f92f2be1f015f68b35addc615558
leticia-chijo <leticia.chijo@gmail.com>
2025-05-25 14:48:07 -0300

    Aula 06

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const redux = require("redux");
+
 // State ------------------------------------------------------
 const initialStateTitle = "";
 const initialStateTasks = [];
@@ -9,7 +11,7 @@ const REMOVE_TASK = "REMOVE_TASK";
 
 // Action Creators => Funções que criam a ação
 function addTitle(title) {
-  return { type: ADD_TITLE, payload: title }; // Actions: objeto => type e payload 
+  return { type: ADD_TITLE, payload: title }; // Actions: objeto => type e payload
 }
 
 function addTask(task) {
@@ -17,5 +19,33 @@ function addTask(task) {
 }
 
 function removeTask(task) {
-  return { type: REMOVE_TASK, payload: taks };
+  return { type: REMOVE_TASK, payload: task };
 }
+
+// Reducers -------------------------------------------------
+// Receber estado atual e a action e retorna como deve ser o novo estado
+// Reducer NÃO ALTERA o estado diretamente, o estado é imutável
+const reducerTitle = (state = initialStateTitle, action) => {
+  switch (action.type) {
+    case ADD_TITLE:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+const reducerTasks = (state = initialStateTasks, action) => {
+  switch (action.type) {
+    case ADD_TASK:
+      return [...state, action.payload];
+    case REMOVE_TASK:
+      return state.filter((t) => t !== action.payload);
+    default:
+      return state;
+  }
+};
+
+const rootReducer = redux.combineReducers({
+  title: reducerTitle,
+  tasks: reducerTasks,
+});
