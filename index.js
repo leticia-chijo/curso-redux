2bab2843630156d167aa8cd0f97c35a47bfc81f9
leticia-chijo <leticia.chijo@gmail.com>
2025-05-25 14:12:18 -0300

    Aula 03

---
+++ index.js
@@ -0,0 +1,10 @@
+// State ------------------------------------------------------
+const initialStateTitle = "";
+const initialStateTasks = [];
+
+function addItemToList(task) {
+  initialStateTasks.push(task);
+}
+
+addItemToList("Lavar louça")
+addItemToList("Estudar redux")
