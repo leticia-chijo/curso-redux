4965f7eccd5019f5bccc3f8fd618976fe70d022f
leticia-chijo <leticia.chijo@gmail.com>
2025-05-25 14:33:48 -0300

    Aula 05

--- index.js
+++ index.js
@@ -2,9 +2,20 @@
 const initialStateTitle = "";
 const initialStateTasks = [];
 
-function addItemToList(task) {
-  initialStateTasks.push(task);
+// Action types ----------------------------------------------
+const ADD_TITLE = "ADD_TITLE";
+const ADD_TASK = "ADD_TASK";
+const REMOVE_TASK = "REMOVE_TASK";
+
+// Action Creators => Funções que criam a ação
+function addTitle(title) {
+  return { type: ADD_TITLE, payload: title }; // Actions: objeto => type e payload 
 }
 
-addItemToList("Lavar louça")
-addItemToList("Estudar redux")
+function addTask(task) {
+  return { type: ADD_TASK, payload: task };
+}
+
+function removeTask(task) {
+  return { type: REMOVE_TASK, payload: taks };
+}
