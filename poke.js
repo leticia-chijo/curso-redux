e70a77a8d02487e0dcd8f0d3cfbccadd03f7a4c9
leticia-chijo <leticia.chijo@gmail.com>
2025-05-25 17:16:04 -0300

    Aula 11

--- poke.js
+++ poke.js
@@ -1,6 +1,11 @@
 const redux = require("redux");
 const reduxLogger = require("redux-logger");
 const logger = reduxLogger.createLogger();
+const axios = require("axios");
+const thunk = require("redux-thunk").thunk;
+
+const POKEMON_AMOUNT = 5;
+const API_URL = `https://pokeapi.co/api/v2/pokemon?limit=${POKEMON_AMOUNT}`;
 
 // State
 const initialState = {
@@ -27,6 +32,16 @@ function fetchError(error) {
   return { type: FETCH_ERROR, payload: error };
 }
 
+// Thunk Action Creator (Async Action)
+function fetchPokemons() {
+  return (dispatch) => {
+    dispatch(fetchRequest());
+    axios
+      .get(API_URL)
+      .then((res) => dispatch(fetchSuccess(res.data.results)))
+      .catch((err) => dispatch(fetchError(err.response.data)));
+  };
+}
 
 // Reducer
 const reducer = (state = initialState, action) => {
@@ -49,5 +64,6 @@ console.log("Initial State", store.getState());
 const unsubscribe = store.subscribe(() => {});
 
 // Dispatch das actions
+store.dispatch(fetchPokemons())
 
 unsubscribe();
